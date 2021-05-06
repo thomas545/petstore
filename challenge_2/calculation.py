6ad7e1a863501ef220d10cf51f6f1d53992b50bb
thomas434 <tomas.temo77@gmail.com>
2021-05-06 15:23:28 +0200

    add validation if there a tie-breakers

--- challenge_2/calculation.py
+++ challenge_2/calculation.py
@@ -1,6 +1,25 @@
 from collections import OrderedDict
 
 
+def sort_same_bidders(data_sorted):
+    new_bidders = []
+
+    for bid, val in data_sorted.items():
+        max_val = max(data_sorted.values())
+        if val == max_val:
+            new_bidders.append(bid)
+    new_bidders.sort()
+    return new_bidders
+
+
+def add_other_bidders(data_sorted, bidders):
+    new_bidders = sort_same_bidders(data_sorted)
+    for bidding in bidders:
+        if bidding not in new_bidders:
+            new_bidders.append(bidding)
+    return new_bidders
+
+
 def calculate_paid_values(bidders):
     if not isinstance(bidders, dict):
         raise ValueError("Bidders must be a dict")
@@ -17,8 +36,9 @@ def calculate_paid_values(bidders):
     bidders = list(data_sorted.keys())
     amounts = list(data_sorted.values())
     amounts.pop(0)
+    new_bidders = add_other_bidders(data_sorted, bidders)
 
-    for bidder in bidders:
+    for bidder in new_bidders:
         if index > len(amounts) - 1:
             results[bidder] = "Lost"
             break
--- challenge_2/execution.py
+++ challenge_2/execution.py
@@ -5,8 +5,8 @@ bidders = {
     "John Smith": 500,
     "Sara Conor": 280,
     "Martin Fowler": 320,
-    "Tom": 1223,
-    "Thomas": 54322,
+    "Tom": 1000,
+    "AThomas": 1000,
 }
 paid_amounts = calculation.calculate_paid_values(bidders)
 
